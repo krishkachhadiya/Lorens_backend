93100258c9d6b4689f85141e4ffca2b98f338305
krish <krishkachhadiya20@gmail.com>
2026-08-03 12:36:36 +0530

    Fix CORS

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:3000",
-      "https://lorens-frontend.vercel.app/",
+      process.env.FRONTEND_URL,
     ],
     credentials: true,
   })
